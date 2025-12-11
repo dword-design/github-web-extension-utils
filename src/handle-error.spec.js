b654da878e05f0fbf811b77b18adbf915dc05a0b
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-12-11 14:56:35 +0100

    fix: foo

--- src/handle-error.spec.js
+++
@@ -1,9 +0,0 @@
-import handleError from './handle-error.js'
-
-export default {
-  'missing name': () => expect(handleError).toThrow("Missing option 'name'"),
-  'missing slug': () =>
-    expect(() => handleError(undefined, { name: 'foo' })).toThrow(
-      "Missing option 'slug'",
-    ),
-}
